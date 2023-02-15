f63929349450334f9ee1c42799d0315f081475f4
shoumitro-cse <shoumitro26@gmail.com>
2023-02-15 21:49:57 +0600

    event loop runtime issue fixed

--- myapp/tasks.py
+++ myapp/tasks.py
@@ -1,7 +1,7 @@
 from celery import shared_task
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
-import time
+import time, asyncio
 from time_progress_task import redis_conn
 
 
@@ -9,11 +9,13 @@ from time_progress_task import redis_conn
 def progress_scheduled_create(group_name, json_data):
     total_task_time = int(json_data["task_time"])
     for second in range(1, total_task_time + 1):
-        async_to_sync(get_channel_layer().group_send)(group_name, {
-            "type": "send_message",
-            'progress_data': (100 * second) / total_task_time,
-            'progress_id': json_data["progress_id"],
-        })
+        event_loop = asyncio.get_event_loop()
+        coroutine_obj = get_channel_layer().group_send(group_name, {
+                "type": "send_message",
+                'progress_data': (100 * second) / total_task_time,
+                'progress_id': json_data["progress_id"],
+            })
+        event_loop.run_until_complete(coroutine_obj)
         time.sleep(1)
     redis_conn.remove_progress_id(json_data["progress_id"])
     return None
